cffb97b99f7ffa810f2347c23a4ab5cbc5f98626
s109e9 <claudia.arias151574@correo.itm.edu.co>
2019-08-31 11:38:35 -0500

    Desarrollo de actividad del seguimiento 1

--- src/seguimientonro1/SeguimientoNro1.java
+++ src/seguimientonro1/SeguimientoNro1.java
@@ -5,6 +5,8 @@
  */
 package seguimientonro1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author s109e9
@@ -15,7 +17,91 @@ public class SeguimientoNro1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nombreMes;
+        //Creación de un objeto Scanner, que permite ingresar información por consola
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.println("************* Seguimiento 1 ****************");
+        System.out.println("\n" + "************* Meses del año****************");
+        System.out.println("\n" + "Ingrese un número: ");
+        //Se captura el número del mes
+        int numeroMes = entradaEscaner.nextInt();
+
+        //Sentencia condiciona swith para saber que mes es
+        switch (numeroMes) {
+            case 1:
+                nombreMes = "Enero";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 4:
+                nombreMes = "Abril";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 6:
+                nombreMes = "Junio";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 7:
+                nombreMes = "Julio";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 9:
+                nombreMes = "Septiembre";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                //Llamado procedimiento para imprimir mes
+                imprimirMes(nombreMes);
+                break;
+            default:
+                System.out.println("El número ingresado no coincide con ningun mes, ingrese un número valido");
+                break;
+        }
     }
-    
+
+    /**
+     * Procedimiento para imprimir mes
+     *
+     */
+    private static void imprimirMes(String mes) {
+
+        System.out.print("El mes equivalente al número ingresado es: " + mes + "\n");
+
+    }
+
 }
