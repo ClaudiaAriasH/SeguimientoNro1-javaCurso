9870b0086c7c3eb2f3b21d4d29eb35246755948f
s109e9 <claudia.arias151574@correo.itm.edu.co>
2019-08-31 11:20:40 -0500

    Segumiento 1 creación proyecto

---
+++ src/seguimientonro1/SeguimientoNro1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seguimientonro1;
+
+/**
+ *
+ * @author s109e9
+ */
+public class SeguimientoNro1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
